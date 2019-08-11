7e0059ee729afdeab6ebda551c719ee4f5399cbc
Ricassio Costa <riksiocosta@gmail.com>
2019-08-11 09:35:13 -0300

    an error in the frame order of the char_knight_run_right's spritesheet fixed

--- Deliverable 4 - Hello World/main.lua
+++ Deliverable 4 - Hello World/main.lua
@@ -5,6 +5,27 @@ display.newText( {text = "Hello World!",x = 320, y = 100, fontSize = 32} )
 display.newText( {text = "Estágio I",x = 320, y = 200, fontSize = 32} )
 display.newText( {text = "Professor Eduardo Mendes",x = 320, y = 260, fontSize = 32} )
 
-char_knight_run_right = display.newImage("image/char_knight_run_right.png", 320, 700)
+charKnightSheetOptions = {
+	width = 84,
+	height = 84,
+	numFrames = 5,
+	sheetContentWidth = 420,
+	sheetContentHeight = 84
+}
 
-char_knight_run_right:scale( 1.5, 1.5 )
+charKnightSheet = graphics.newImageSheet( "image/char_knight_run_right.png", charKnightSheetOptions )
+
+knightSequenceData = {
+	name = "char_knight",
+	start = 2,
+	count = 5,
+	time = 500,
+	loopCount = 0,
+	loopDirection = "forward"
+}
+
+newCharKnight = display.newSprite( charKnightSheet, knightSequenceData )
+newCharKnight.x = 320
+newCharKnight.y = 700
+
+newCharKnight:play( )
