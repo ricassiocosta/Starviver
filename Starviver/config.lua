17e505412959b9b99639147177d6c54cad65fb31
ricássio da silva costa - 1818454 <1818454@7setlabs.local>
2019-10-17 19:01:07 -0300

    Reorganized Directory Trees and all draft folders removed

--- Starviver/config.lua
+++
@@ -1,8 +0,0 @@
-application = {
-	content = {
-		width = 1080,
-		height = 1920,
-		scale = "letterBox",
-		fps = 60
-	}
-}
--- Starviver/main.lua
+++
@@ -1,9 +0,0 @@
-local gameloop = require("gameloop")
-
-gameloop:init();
-
-local function gameLoop()
-    gameloop:run()
-end
-
-Runtime:addEventListener("enterFrame", gameLoop)
