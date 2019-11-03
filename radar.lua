207ae512eebe999187466fcd2c721cb33ba4b231
Ricassio Costa <riksiocosta@gmail.com>
2019-11-03 15:32:26 -0300

    Added outline to radar to make it more visible

--- radar.lua
+++ radar.lua
@@ -17,7 +17,9 @@ function radar.class:__init(_rootObj)
   self.rootObject = _rootObj;
 
   self.radarBackground = display.newCircle(275, 275, 225);
-  self.radarBackground:setFillColor(0,0,0, 0.65);
+  self.radarBackground:setFillColor(0,0.2,0, 0.65);
+  self.radarBackground.strokeWidth = 5
+  self.radarBackground.stroke = {0.1, 0.7, 0.2}
   self.radarTri = display.newImageRect("imgs/radar-triangle.png", 50, 50);
   self.radarTri.x = 275; self.radarTri.y = 275;
   self.radarTri.rotation = self.rootObject.rotation;
